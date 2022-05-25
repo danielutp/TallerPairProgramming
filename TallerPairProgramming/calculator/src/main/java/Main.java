ade928eb6d8cfaa4657f71d74a363b3d95dacf2b
yeferson1221 <alejandro.yandd@gmail.com>
2022-05-25 18:27:58 -0500

    se hace la documentacion completa terminado

--- TallerPairProgramming/calculator/src/main/java/Main.java
+++ TallerPairProgramming/calculator/src/main/java/Main.java
@@ -3,9 +3,29 @@ import operation.Calculator;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
-
+/**
+ * Ejercicio Taller Pair programming
+ *         contien la clase main  que ejecuta el proyecto
+ *
+ * @version 1.0.0 2022-05-25
+ * @author Daniel Steven Gil Cruz <danistcruz@gmail.com> - Yeferson Valencia (alejandro.yandd@gmail.com)
+ * @since 1.0.0
+ */
 public class Main{
-
+    /**
+     *        Contiene el metodo main  instanciamos logger calculator y scaner
+     *        Scanner: poder capturar  lo que ingresa el usuario en los number one y numerberTwo
+     *        logger: mostrar mensaje en consola
+     *        Calculator: instancia el objeto para usar sus metedos
+     *             case 1 -> calculator.addition();
+     *             case 2 -> calculator.subtraction();
+     *             case 3 -> calculator.multiplied();
+     *             case 4 -> calculator.split();
+     *
+     * @version 1.0.0 2022-05-25
+     * @author Daniel Steven Gil Cruz (danistcruz@gmail.com) - Yeferson Valencia (alejandro.yandd@gmail.com)
+     * @since 1.0.0
+     */
     public static void main(String[] args){
         Logger log = Logger.getLogger(Calculator.class.getName());
         Calculator calculator = new Calculator();
--- TallerPairProgramming/calculator/src/main/java/operation/Calculator.java
+++ TallerPairProgramming/calculator/src/main/java/operation/Calculator.java
@@ -3,11 +3,33 @@ package operation;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
+/**
+ * Ejercicio Taller Pair programming
+ *         contien la clase calculator  con sus metodos
+ *                    1 -> addition();
+ *      *             2 -> subtraction();
+ *      *             3 -> multiplied();
+ *      *             4 -> split();
+ *
+ * @version 1.0.0 2022-05-25
+ * @author Daniel Steven Gil Cruz <danistcruz@gmail.com> - Yeferson Valencia (alejandro.yandd@gmail.com)
+ * @since 1.0.0
+ */
 public class Calculator {
     private int numberOne;
     private int numberTwo;
 
     private final Logger log =Logger.getLogger(Calculator.class.getName());
+
+    /**
+     * 1 -> intro(); este nos permite agregar los numeros
+     *               para posterios mente darle valos
+     *               a los atributos numberOne y numberTwo
+     *
+     * @version 1.0.0 2022-05-25
+     * @author Daniel Steven Gil Cruz <danistcruz@gmail.com> - Yeferson Valencia (alejandro.yandd@gmail.com)
+     * @since 1.0.0
+     */
     public void intro(){
         Scanner in = new Scanner(System.in);
         log.info("ingresar Numero 1: ");
@@ -15,24 +37,53 @@ public class Calculator {
         log.info("ingresar Numero 2: ");
         numberTwo= in.nextInt();
     }
+
+    /**
+     * 1 -> addition(); hace la suma entre numberOne y numberTwo
+     *
+     * @version 1.0.0 2022-05-25
+     * @author Daniel Steven Gil Cruz <danistcruz@gmail.com> - Yeferson Valencia (alejandro.yandd@gmail.com)
+     * @since 1.0.0
+     */
     public void addition(){
         int result=(numberOne+numberTwo);
         String resultado = "El resultado de la suma es : " + result;
         log.info(resultado);
     }
 
+    /**
+     * 1 -> addition(); hace la resta entre numberOne y numberTwo
+     *
+     * @version 1.0.0 2022-05-25
+     * @author Daniel Steven Gil Cruz <danistcruz@gmail.com> - Yeferson Valencia (alejandro.yandd@gmail.com)
+     * @since 1.0.0
+     */
     public void subtraction(){
         int result=(numberOne-numberTwo);
         String resultado = "El resultado de la resta: " + result;
         log.info(resultado);
     }
 
+    /**
+     * 1 -> addition(); hace la multiplicacion entre numberOne y numberTwo
+     *
+     * @version 1.0.0 2022-05-25
+     * @author Daniel Steven Gil Cruz <danistcruz@gmail.com> - Yeferson Valencia (alejandro.yandd@gmail.com)
+     * @since 1.0.0
+     */
     public void multiplied(){
         int result=(numberOne*numberTwo);
         String resultado = "El resultado de la multiplicacion : " + result;
         log.info(resultado);
     }
 
+    /**
+     * 1 -> addition(); hace la division entre numberOne y numberTwo
+     *
+     * @version 1.0.0 2022-05-25
+     * @author Daniel Steven Gil Cruz <danistcruz@gmail.com> - Yeferson Valencia (alejandro.yandd@gmail.com)
+     * @since 1.0.0
+     */
     public void split(){
         int result=(numberOne/numberTwo);
         String resultado = "El resultado  de la division : " + result;
