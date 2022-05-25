710866f39026c84fb3f3fe25571af1b3a82e6b88
yeferson1221 <alejandro.yandd@gmail.com>
2022-05-25 17:16:19 -0500

    se crean tdoos los metodos sumar restar multiplicar divivir ingrar valores

--- TallerPairProgramming/calculator/src/main/java/Main.java
+++ TallerPairProgramming/calculator/src/main/java/Main.java
@@ -1,2 +1,6 @@
+import operation.Calculator;
+
+
 public class Main {
+
 }
--- TallerPairProgramming/calculator/src/main/java/operation/Calculator.java
+++ TallerPairProgramming/calculator/src/main/java/operation/Calculator.java
@@ -1,7 +1,35 @@
 package operation;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Calculator {
     private int numberOne;
     private int numberTwo;
+
+    private  Logger log =Logger.getLogger(Calculator.class.getName());
+    public void intro(){
+        Scanner in = new Scanner(System.in);
+        log.info("ingresar Numero 1: ");
+        numberOne=in.nextInt();
+        log.info("ingresar Numero 2: ");
+        numberTwo= in.nextInt();
+    }
+    public void addition(){
+        int result=(numberOne+numberTwo);
+
+    }
+
+    public void subtraction(){
+        int result=(numberOne-numberTwo);
+    }
+
+    public void multiplied(){
+        int result=(numberOne*numberTwo);
+    }
+
+    public void split(){
+        int result=(numberOne+numberTwo);
+    }
 
 }
