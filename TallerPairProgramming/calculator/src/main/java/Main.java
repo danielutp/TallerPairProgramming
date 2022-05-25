28d9dd3d722040fa130903c067bbd268d3bccdf9
danielgil <danistcruz@gmail.com>
2022-05-25 17:51:45 -0500

    Se crea el main con el switch case para las opciones de operaciones

--- TallerPairProgramming/calculator/src/main/java/Main.java
+++ TallerPairProgramming/calculator/src/main/java/Main.java
@@ -1,6 +1,30 @@
 import operation.Calculator;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
 
-public class Main {
 
+public class Main{
+
+    public static void main(String[] args){
+        Logger log = Logger.getLogger(Calculator.class.getName());
+        Calculator calculator = new Calculator();
+        Scanner in = new Scanner(System.in);
+        calculator.intro();
+        log.info("""
+                Ingrese que desea hacer con los numeros ingresados:\s
+                1. Suma\s
+                2. Resta\s
+                3. Multiplicacion\s
+                4. Division\s
+                """);
+        int option =in.nextInt();
+        switch (option) {
+            case 1 -> calculator.addition();
+            case 2 -> calculator.subtraction();
+            case 3 -> calculator.multiplied();
+            case 4 -> calculator.split();
+            default -> log.info("Error");
+        }
+    }
 }
--- TallerPairProgramming/calculator/src/main/java/operation/Calculator.java
+++ TallerPairProgramming/calculator/src/main/java/operation/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
     private int numberOne;
     private int numberTwo;
 
-    private  Logger log =Logger.getLogger(Calculator.class.getName());
+    private final Logger log =Logger.getLogger(Calculator.class.getName());
     public void intro(){
         Scanner in = new Scanner(System.in);
         log.info("ingresar Numero 1: ");
@@ -17,19 +17,25 @@ public class Calculator {
     }
     public void addition(){
         int result=(numberOne+numberTwo);
-
+        String resultado = "El resultado de la suma es : " + result;
+        log.info(resultado);
     }
 
     public void subtraction(){
         int result=(numberOne-numberTwo);
+        String resultado = "El resultado de la resta: " + result;
+        log.info(resultado);
     }
 
     public void multiplied(){
         int result=(numberOne*numberTwo);
+        String resultado = "El resultado de la multiplicacion : " + result;
+        log.info(resultado);
     }
 
     public void split(){
-        int result=(numberOne+numberTwo);
+        int result=(numberOne/numberTwo);
+        String resultado = "El resultado  de la division : " + result;
+        log.info(resultado);
     }
-
 }
